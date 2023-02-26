8c614105f64e4670f88a70a3470c5346ea713402
ZedViO <kjollerd@gmail.com>
2023-02-26 19:57:40 +0300

    CLang-Formatted Code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,13 @@
-#include <iostream>//*If I have just added some comms...*
+#include <iostream> //*If I have just added some comms...*
 #include <string>
 
-int main () {//Something here...
-std::cout << "Pls your name" << std::endl;
-std::string name = "";
-std::cin >> name;
-std::cout << "Hello world from " + name << std::endl;   //Maybe here too...
-return 0;
+int
+main()
+{ // Something here...
+  std::cout << "Pls your name" << std::endl;
+  std::string name = "";
+  std::cin >> name;
+  std::cout << "Hello world from " + name << std::endl; // Maybe here too...
+  return 0;
 }
+
